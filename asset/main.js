650c841748d13e7d63bb2a7e9bfcfee8ddbd71cd
Sulca2099 <Lucasaswenson@gmail.com>
2026-03-26 11:02:48 -0700

    Update main.js

--- asset/main.js
+++ asset/main.js
@@ -1,10 +1,12 @@
 function darkMode(){}
 function showBlogEntries(){
+    const thisweb = document.getElementById('blogbody');
+    thisweb.innerHTML="Loading";
     try{
         alert("testing...");
         const data = await fetch("https://sulca2099.github.io/Webpageprofesional/blog/entries.json");
         const jsondata = (await data.json).link;
-        const thisweb = document.getElementById('blogbody');
+        thisweb.innerHTML="";
         for(const i of jsondata.link){
             thisweb.innerHTML+=`<div class="blogelement"><div class="largededication">${(await (await fetch(i)).json).title}</div>${(await (await fetch(i)).json).description}<br>{${(await (await fetch(i)).json).date}}<div></div>`;
         }
